5c31f4016668c5c1f3a6bef5d5a5835073a0566b
roman <rgnevashev@gmail.com>
2016-02-18 14:54:08 +0300

    update

--- package.js
+++ package.js
@@ -1,15 +1,16 @@
 Package.describe({
   name: 'jkbrzt:rrule',
-  version: '2.2.0',
+  version: '2.2.0_1',
   summary: 'JavaScript library for working with recurrence rules for calendar dates.',
   git: 'git://github.com/jkbrzt/rrule.git',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
+  api.export('RRule');
   api.addFiles([
     "lib/rrule.js",
-    "lib/nlp.js"
+    "lib/nlp.js",
+    "export.js"
   ], ['client','server']);
-  api.export('RRule');
 });

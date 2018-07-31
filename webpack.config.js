03fa0d422e32a309491d702cd84902df6fd3c5ce
Nick Arora <arora.nick@gmail.com>
2018-07-31 00:42:20 -0700

    clean up formatting on webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,65 @@
-const webpack = require('webpack')
-const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require("webpack");
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const paths = {
   demo: {
-    styles: path.resolve(__dirname, 'demo/demo.css'),
-    template: path.resolve(__dirname, 'demo/index.html'),
+    styles: path.resolve(__dirname, "demo/demo.css"),
+    template: path.resolve(__dirname, "demo/index.html")
   },
-  dist: path.resolve(__dirname, 'dist'),
-}
+  dist: path.resolve(__dirname, "dist")
+};
 
-module.exports = [{
+const rruleConfig = {
   entry: {
-    index: './src/index.js'
+    index: "./src/index.js"
   },
   output: {
-    filename: 'rrule.js',
+    filename: "rrule.js",
     path: paths.dist,
-    library: 'rrule',
-    libraryTarget: 'umd',
-    globalObject: 'typeof self !== \'undefined\' ? self : this'
+    library: "rrule",
+    libraryTarget: "umd",
+    globalObject: "typeof self !== 'undefined' ? self : this"
   },
-  devtool: 'source-map',
-  mode: 'production'
-}, {
+  devtool: "source-map",
+  mode: "production"
+};
+
+const demoConfig = {
   entry: {
-    demo: './demo/demo.coffee',
+    demo: "./demo/demo.coffee"
   },
   module: {
     rules: [
       {
         exclude: /node_modules/,
-        loader: 'coffee-loader',
-        test: /\.coffee$/,
+        loader: "coffee-loader",
+        test: /\.coffee$/
       }
-    ],
+    ]
   },
   output: {
-    filename: 'demo.js',
+    filename: "demo.js",
     path: paths.dist
   },
   plugins: [
     new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery'
+      $: "jquery",
+      jQuery: "jquery"
     }),
-    new CopyWebpackPlugin([{
-      from: paths.demo.styles,
-      to: paths.dist,
-    }]),
+    new CopyWebpackPlugin([
+      {
+        from: paths.demo.styles,
+        to: paths.dist
+      }
+    ]),
     new HtmlWebpackPlugin({
-      template: paths.demo.template,
+      template: paths.demo.template
     })
   ],
-  devtool: 'source-map',
-  mode: 'production'
-}];
+  devtool: "source-map",
+  mode: "production"
+};
+
+module.exports = [rruleConfig, demoConfig];

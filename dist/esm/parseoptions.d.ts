0533319f27b8b00ed2c0df0761b4fc514fedbc47
David Golightly <davigoli@gmail.com>
2018-10-01 01:02:39 -0700

    remove dist/

--- dist/esm/parseoptions.d.ts
+++
@@ -1,7 +0,0 @@
-import { Options, ParsedOptions } from './types';
-import dateutil from './dateutil';
-export declare function initializeOptions(options: Partial<Options>): Partial<Options>;
-export declare function parseOptions(options: Partial<Options>): {
-    parsedOptions: ParsedOptions;
-    timeset: dateutil.Time[] | null;
-};

815a507bd454b5f99345276d723e58af1e418ef3
Tim Shaker <tim@tmshkr.com>
2022-04-02 17:16:15 -0700

    apply timezone offset to test dtstart UTC times

--- test/optionstostring.test.ts
+++ test/optionstostring.test.ts
@@ -7,14 +7,14 @@ describe('optionsToString', () => {
   it('serializes valid single lines of rrules', function () {
     const expectations: ([ Partial<Options>, string ][]) = [
       [{ freq: RRule.WEEKLY, until: new Date(Date.UTC(2010, 0, 1, 0, 0, 0)) }, 'RRULE:FREQ=WEEKLY;UNTIL=20100101T000000Z' ],
-      [{ dtstart: new Date(Date.UTC(1997, 8, 2, 9, 0, 0)), tzid: 'America/New_York' }, 'DTSTART;TZID=America/New_York:19970902T090000' ],
+      [{ dtstart: new Date(Date.UTC(1997, 8, 2, 13, 0, 0)), tzid: 'America/New_York' }, 'DTSTART;TZID=America/New_York:19970902T090000' ],
       [
         { dtstart: new Date(Date.UTC(1997, 8, 2, 9, 0, 0)), freq: RRule.WEEKLY },
         'DTSTART:19970902T090000Z\n' +
         'RRULE:FREQ=WEEKLY'
       ],
       [
-        { dtstart: new Date(Date.UTC(1997, 8, 2, 9, 0, 0)), tzid: 'America/New_York', freq: RRule.WEEKLY },
+        { dtstart: new Date(Date.UTC(1997, 8, 2, 13, 0, 0)), tzid: 'America/New_York', freq: RRule.WEEKLY },
         'DTSTART;TZID=America/New_York:19970902T090000\n' +
         'RRULE:FREQ=WEEKLY'
       ]

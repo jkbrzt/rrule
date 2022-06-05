d8e3e1d4b95019283d4b9717b68edf184844fb9d
Tim Shaker <tim@tmshkr.com>
2022-06-05 10:37:11 -0700

    add dayjs

---
+++ src/lib/dayjs.ts
@@ -0,0 +1,5 @@
+export const dayjs = require('dayjs')
+const utc = require('dayjs/plugin/utc')
+const timezone = require('dayjs/plugin/timezone') // dependent on utc plugin
+dayjs.extend(utc)
+dayjs.extend(timezone)
